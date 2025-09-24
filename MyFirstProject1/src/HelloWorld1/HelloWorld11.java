c1a70aa0d718fa67614fda81de3d5bae513efc0d
zulfaizakbar <zulfaizakbar@gmail.com>
2025-09-24 18:03:02 +0800

    First Commit

---
+++ MyFirstProject1/src/HelloWorld1/HelloWorld11.java
@@ -0,0 +1,11 @@
+package HelloWorld1;
+
+public class HelloWorld11 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello...World!!!");
+	}
+
+}
